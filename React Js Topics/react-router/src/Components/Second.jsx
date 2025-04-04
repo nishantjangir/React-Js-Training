26003a5aaf71b9054dca92bb5fab6589bd407e0f
nishantjangir <nishant.jangir@applifare.com>
2025-04-04 09:47:54 +0530

    Delete .gitignore

--- React Js Topics/react-router/src/Components/Second.jsx
+++
@@ -1,8 +0,0 @@
-export default function Second({ time }) {
-  return (
-    <>
-      <h1>{time}</h1>
-      <input />
-    </>
-  );
-}
--- React Js Topics/react-router/src/Context/counterContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const counterContext = createContext();
-
-export default counterContext;
